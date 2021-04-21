970608f3e341baf621445ef18afca77021115187
nikkaushal <nikkaushal@gmail>
2021-04-20 21:42:15 -0400

    jenkins

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -7,18 +7,5 @@ def warning(message) {
 }
 
 def terraform(message) {
-    pipeline {
-        agent any
-        stages {
-            stage('Shared Library Demo') {
-                steps {
-                    script {
-                        print 'Hello'
-
-                    }
-                }
-            }
-
-        }
-    }
+    echo "Nothing : ${message}"
 }
