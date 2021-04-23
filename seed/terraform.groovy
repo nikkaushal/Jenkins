8c0ed31d3f78a5278f4ac0a5b69b03d2a50e1502
nikkaushal <nikkaushal@gmail>
2021-04-22 22:17:35 -0400

    jenkins

--- seed/terraform.groovy
+++ seed/terraform.groovy
@@ -45,3 +45,24 @@ pipelineJob('Terraform-Releases/FRONTEND') {
         }
     }
 }
+
+pipelineJob('Terraform-Releases/ALB') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/nikkaushal/terraform-alb.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/master')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
