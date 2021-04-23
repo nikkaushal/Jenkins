d8a7023439cc6ebb2faa5ece7f6d0d598bbd8902
nikkaushal <nikkaushal@gmail>
2021-04-22 22:18:42 -0400

    jenkins

--- seed/terraform.groovy
+++ seed/terraform.groovy
@@ -57,7 +57,7 @@ pipelineJob('Terraform-Releases/ALB') {
                 }
                 'branches' {
                     'hudson.plugins.git.BranchSpec' {
-                        'name'('*/master')
+                        'name'('*/main')
                     }
                 }
             }
