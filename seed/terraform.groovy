1cf4696e1f61baa2e55330b3a58d9ae63d7c4643
nikkaushal <nikkaushal@gmail>
2021-04-22 21:39:08 -0400

    jenkins

---
+++ seed/terraform.groovy
@@ -0,0 +1,26 @@
+///
+folder('Terraform-Releases') {
+    displayName('Terraform-Releases')
+    description('Terraform-Releases')
+}
+
+pipelineJob('Terraform-Releases/VPC') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/nikkaushal/terraform-vpc.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
